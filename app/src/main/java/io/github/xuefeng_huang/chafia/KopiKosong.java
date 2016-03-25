449025416f7fa230facddfd4b818b425169150f0
Huang xuefeng <timhxf@gmail.com>
2016-03-25 15:23:11 +0800

    add new activity to show kopi pic

--- app/src/main/java/io/github/xuefeng_huang/chafia/KopiKosong.java
+++ app/src/main/java/io/github/xuefeng_huang/chafia/KopiKosong.java
@@ -1,20 +1,23 @@
 package io.github.xuefeng_huang.chafia;
 
+/**
+ * Created by xuefeng huang on 3/25/2016.
+ */
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class KopiKosong extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.kopi_kosong);
     }
 
-    public void kopiKosongClick(View v) {
-        Intent intent = new Intent(this, KopiKosong.class);
+    public void backToMain(View v) {
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
