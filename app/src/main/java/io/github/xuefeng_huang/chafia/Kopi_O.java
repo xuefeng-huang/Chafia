ff543cdae1c795362af57b79fa5b126b40324e6a
Huang xuefeng <timhxf@gmail.com>
2016-03-27 17:23:42 +0800

    add clickable function to all cards

--- app/src/main/java/io/github/xuefeng_huang/chafia/Kopi_O.java
+++ app/src/main/java/io/github/xuefeng_huang/chafia/Kopi_O.java
@@ -1,18 +1,15 @@
 package io.github.xuefeng_huang.chafia;
 
-/**
- * Created by xuefeng huang on 3/25/2016.
- */
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 
-public class KopiKosong extends AppCompatActivity {
+public class Kopi_O extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.kopi_kosong);
+        setContentView(R.layout.activity_kopi__o);
     }
 
     public void backToMain(View v) {
--- app/src/main/java/io/github/xuefeng_huang/chafia/MainActivity.java
+++ app/src/main/java/io/github/xuefeng_huang/chafia/MainActivity.java
@@ -1,6 +1,7 @@
 package io.github.xuefeng_huang.chafia;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -15,6 +16,32 @@ public class MainActivity extends AppCompatActivity {
 
     public void kopiKosongClick(View v) {
         Intent intent = new Intent(this, KopiKosong.class);
+        startActivity(intent);
+    }
+
+    public void kopiOClick(View view) {
+        Intent intent = new Intent(this, Kopi_O.class);
+        startActivity(intent);
+    }
+
+    public void kopiOSiewDaiClick(View view) {
+        Intent intent = new Intent(this, kopiOSiewDai.class);
+        startActivity(intent);
+    }
+
+    public void kopiClick(View view) {
+        Intent intent = new Intent(this, Kopi.class);
+        startActivity(intent);
+    }
+
+    public void kopiGahDaiClick(View view) {
+        Intent intent = new Intent(this, KopiGahDai.class);
+        startActivity(intent);
+    }
+
+    public void learnMoreClick(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://en.wikipedia.org/wiki/Kopi_tiam"));
         startActivity(intent);
     }
 }
